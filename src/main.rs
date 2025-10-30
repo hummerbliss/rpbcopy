690cfa16a16f48e52e420765121e9b4670bf93dd
Saikiran Madugula <foobar@hummerbliss.com>
2025-10-31 01:22:30 +0530

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 extern crate clap;
 
-use std::fs;
 use clap::Parser;
+use exitcode;
+use std::fs;
 use std::io;
 use std::io::Read;
 use std::io::Write;
-use exitcode;
-
 
 #[derive(Parser)]
 #[clap(author, version, about = "Copy files' content to clipboard.", long_about = None)]
@@ -19,19 +18,18 @@ struct Cli {
 /// Send content to clipboard using OSC52 escape sequence
 /// Works with iTerm2, Ghostty, and other terminals that support OSC52
 fn copy_to_clipboard_osc52(content: &str) -> Result<(), Box<dyn std::error::Error>> {
-    use base64::{Engine as _, engine::general_purpose::STANDARD};
-    
+    use base64::{engine::general_purpose::STANDARD, Engine as _};
+
     // Encode the content in base64
     let encoded = STANDARD.encode(content.as_bytes());
-    
+
     // Output OSC52 escape sequence: ESC ] 52 ; c ; <base64> BEL
     // \033 is ESC, \a is BEL (ASCII 7)
     print!("\x1b]52;c;{}\x07", encoded);
     io::stdout().flush()?;
-    
+
     Ok(())
 }
-
 
 fn main() {
     let stdin = io::stdin();
@@ -52,11 +50,15 @@ fn main() {
     for filename in &files {
         let mut tmp = String::new();
         if "-" == filename {
-            stdin.lock().read_to_string(&mut tmp)
+            stdin
+                .lock()
+                .read_to_string(&mut tmp)
                 .expect("Read from stdin failed");
         } else {
-            tmp = fs::read_to_string(filename)
-                .expect(&*format!("Something went wrong reading the file '{}'", filename));
+            tmp = fs::read_to_string(filename).expect(&*format!(
+                "Something went wrong reading the file '{}'",
+                filename
+            ));
         }
 
         if !contents.ends_with('\n') && !contents.is_empty() {
@@ -65,6 +67,5 @@ fn main() {
         contents.push_str(&tmp);
     }
 
-    copy_to_clipboard_osc52(&contents)
-        .expect("Failed to copy to clipboard");
+    copy_to_clipboard_osc52(&contents).expect("Failed to copy to clipboard");
 }
